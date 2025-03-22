6ae7d9d224eedf426045df8a878093a52133c515
Vedant Cheulkar <vedantcheulkar29032004@gmail.com>
2025-03-22 10:36:30 +0530

    added CommunityScreen, HomeScreen, ProfileScreen, SOSScreen, SplashScreen, TrackMeScreen

---
+++ RakshaSetu/src/screens/SOSScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SOSScreen = () => {
+  return (
+    <View>
+      <Text>SOSScreen</Text>
+    </View>
+  )
+}
+
+export default SOSScreen
---
+++ RakshaSetu/src/screens/TrackMeScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const TrackMeScreen = () => {
+  return (
+    <View>
+      <Text>TrackMeScreen</Text>
+    </View>
+  )
+}
+
+export default TrackMeScreen
