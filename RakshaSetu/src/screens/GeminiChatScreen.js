640cf622ff307fd277eed751115a7487ecf9773a
Soham6888 <kingragnarlothbrok6888@gmail.com>
2025-03-22 21:46:51 +0530

    feat: add GeminiChatScreen component

---
+++ RakshaSetu/src/screens/GeminiChatScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const GeminiChatScreen = () => {
+  return (
+    <View>
+      <Text>GeminiChatScreen</Text>
+    </View>
+  )
+}
+
+export default GeminiChatScreen
