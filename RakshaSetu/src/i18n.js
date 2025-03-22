5a21ee8902132620fcfb37d0e203f32d9a6b6df0
Pratik Nitin Pisal <pisalpratik2004@gmail.com>
2025-03-22 21:31:10 +0530

    feat: add multilingual support with Hindi, Marathi, Gujarati, Tamil, Telugu, Kannada, and Punjabi translations

--- RakshaSetu/src/i18n.js
+++ RakshaSetu/src/i18n.js
@@ -1,7 +1,13 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import en from './locales/en.json';
-
+import hi from './locales/hi.json';
+import mr from './locales/mr.json';
+import gu from './locales/gu.json';
+import ta from './locales/ta.json';
+import te from './locales/te.json';
+import kn from './locales/kn.json';
+import pa from './locales/pa.json';
 
 i18next
   .use(initReactI18next)
@@ -10,6 +16,13 @@ i18next
     fallbackLng: 'en',
     resources: {
       en: { translation: en },
+      hi: { translation: hi },
+      mr: { translation: mr },
+      gu: { translation: gu },
+      ta: { translation: ta },
+      te: { translation: te },
+      kn: { translation: kn },
+      pa: { translation: pa }
     }
   });
 
