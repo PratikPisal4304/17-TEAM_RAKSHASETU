55ae4e1fa3167e7ab85e39162f381833c5bab749
Soham6888 <kingragnarlothbrok6888@gmail.com>
2025-03-22 18:19:37 +0530

    feat: add MyPostsScreen and MyReportsScreen components

---
+++ RakshaSetu/src/screens/MyPostsScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const MyPostsScreen = () => {
+  return (
+    <View>
+      <Text>MyPostsScreen</Text>
+    </View>
+  )
+}
+
+export default MyPostsScreen
