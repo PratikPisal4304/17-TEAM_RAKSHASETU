d6dd9f9a962fbbfe7155cb799cee0f3662879cb2
Pratik Nitin Pisal <pisalpratik2004@gmail.com>
2025-03-23 05:39:52 +0530

    Add uuid package and refactor imports in App.js; update LiveLocationScreen

--- RakshaSetu/App.js
+++ RakshaSetu/App.js
@@ -1,10 +1,11 @@
+import 'react-native-get-random-values'; // Add this line at the very top
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import Routes from "./src/routes";
 import { UserProvider } from "./src/context/UserContext";
 import { ShakeDetectionProvider } from "./src/context/ShakeDetectionContext";
-import { VoiceDetectionContext, VoiceDetectionProvider } from "./src/context/VoiceDetectionContext";
+import { VoiceDetectionProvider } from "./src/context/VoiceDetectionContext";
 import './src/i18n'; // Ensure this file exists
 
 const App = () => {
@@ -12,11 +13,11 @@ const App = () => {
     <UserProvider>
       <ShakeDetectionProvider>
         <VoiceDetectionProvider>
-        <SafeAreaProvider>
-          <NavigationContainer>
-            <Routes />
-          </NavigationContainer>
-        </SafeAreaProvider>
+          <SafeAreaProvider>
+            <NavigationContainer>
+              <Routes />
+            </NavigationContainer>
+          </SafeAreaProvider>
         </VoiceDetectionProvider>
       </ShakeDetectionProvider>
     </UserProvider>
